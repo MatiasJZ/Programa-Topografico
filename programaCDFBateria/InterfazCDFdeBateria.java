111c1a74818602c8247392f081b5533d47fbf0b3
MatiasJZ <matii.juarz5@gmail.com>
2025-08-27 10:49:47 -0300

    Clases Blanco, Serie y Grupo. Falta corregir los imports del JMapViewer

--- programaCDFBateria/InterfazCDFdeBateria.java
+++ programaCDFBateria/InterfazCDFdeBateria.java
@@ -5,12 +5,9 @@ public class InterfazCDFdeBateria extends JPanel {
 
     private String idCDF;
 
-    public InterfazCDFdeBateria(InterfazUsuario padre) {
+    public InterfazCDFdeBateria() {
         setLayout(new BorderLayout());
         setBackground(new Color(40, 55, 40)); // Verde militar
-        JButton volver = new JButton("Volver");
-        volver.addActionListener(e -> padre.mostrarPantalla("principal"));
-        add(volver, BorderLayout.SOUTH);
     }
 
     public void setID(String id){
